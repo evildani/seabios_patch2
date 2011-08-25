c0acd67eb0b4469af171fb896cad70c6712b7111
Daniel Castro <evil.dani@gmail.com>
2011-08-26 08:43:41 +0900

    Xenstore: read, write and directory opertaion
    Created wrappers for read, write and directory to hidde
    xenbus_send functionality.
    
    Signed-off-by: Daniel Castro <evil.dani@gmail.com>

--- src/xen-xs.h
+++ src/xen-xs.h
@@ -1,6 +1,8 @@
 #ifndef _XEN_XS_H
 #define _XEN_XS_H
 
+char * add_string(char * a,char *b);
+char * strconcat(char *dest, const char *src);
 void xenbus_setup(void);
 char * xenstore_read(char *path);
 char * xenstore_directory(char *path, u32 *ans_len);
